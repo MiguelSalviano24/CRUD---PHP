d1eb8245916588c4c7e3ba7fdc36e409fa5cc57e
miguel salviano <miguelcf.tg@gmail.com>
2025-04-07 10:27:07 -0300

    feat: visualize button added

--- index.php
+++ index.php
@@ -31,11 +31,35 @@
                                 <a href="usuario-create.php" class="btn btn-primary float-end">Adicionar usuário</a>
                             </h4>
                         </div>
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                        <th>Data de nascimento</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>teste</td>
+                                        <td>teste@email.com</td>
+                                        <td>09/01/2001</td>
+                                        <td>
+                                            <a href="" class="btn btn-secondary btn-sm">Visualizar</a>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </main>
     <footer>
         <!-- place footer here -->
